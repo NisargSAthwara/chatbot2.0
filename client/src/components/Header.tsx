33c63378795248cfc4055fb79e388e12438f637d
NisargSAthwara <nisargsathwara16@gmail.com>
2025-05-27 18:49:49 +0530

    Resolved api100 error

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="relative flex items-center justify-between px-8 py-5 bg-gradient-to-r from-[#4A3F71] to-[#5E507F] z-10">
-            <div className="absolute inset-0 bg-[url('/api/placeholder/100/100')] opacity-5 mix-blend-overlay"></div>
+            <div className="absolute inset-0 bg-gray-100 opacity-5 mix-blend-overlay"></div>
             <div className="absolute bottom-0 left-0 right-0 h-px bg-gradient-to-r from-transparent via-white/20 to-transparent"></div>
 
             <div className="flex items-center relative">
